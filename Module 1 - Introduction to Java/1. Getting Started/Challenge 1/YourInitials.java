25cec5cc252e9c7158bc3b8d81c761a13d32573f
CrimsonCoder42 <nostro37@gmail.com>
2022-09-02 09:56:58 -0400

    challenge finished

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,131 @@
 public class YourInitials {
     public static void main(String[] args) {
-        //print initials here. 
+        System.out.print("D");
+        System.out.print("D");
+        System.out.print("D");
+        System.out.print("D");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+
+        System.out.println("");
+
+        System.out.print("D");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("D");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print(" ");
+        System.out.print("A");
+
+        System.out.println("");
+
+        System.out.print("D");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("D");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("A");
+
+        System.out.println("");
+
+        System.out.print("D");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("D");
+        
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print("A");
+        System.out.print("A");
+        System.out.print("A");
+        System.out.print("A");
+
+        System.out.println("");
+
+        System.out.print("D");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("D");
+       
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("A");
+
+        System.out.println("");
+
+        System.out.print("D");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("D");
+        
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("A");
+
+        System.out.println("");
+
+
+        System.out.print("D");
+        System.out.print("D");
+        System.out.print("D");
+        System.out.print("D");
+        
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+
+        System.out.print("A");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("A");
+
+        System.out.println("");
+
     }
 }
--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -9,3 +9,11 @@
                                               // "Java > Python."
                                               // "I spilled Java all over my paper."
                                               // "My dog ate my Java." 
+public class FirstProgram{
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+}
