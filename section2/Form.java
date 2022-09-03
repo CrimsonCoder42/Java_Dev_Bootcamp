93bb18a3b1864765d90dbff112052bc9839f04a2
CrimsonCoder42 <nostro37@gmail.com>
2022-09-03 08:33:26 -0400

    typer: int and long

---
+++ section2/Form.java
@@ -0,0 +1,9 @@
+public class Form{
+    public static void main(String[] args) {
+        // char takes up less memory and is faster 
+        char gender = 'F';
+        
+        System.out.println("Gender: " + gender);
+
+    }
+}
