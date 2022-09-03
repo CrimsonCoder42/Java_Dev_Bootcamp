a13c52b7766987626a7db76934b2177259c8cfc8
CrimsonCoder42 <nostro37@gmail.com>
2022-09-03 07:20:59 -0400

    workbook 2.1

--- section2/Variables.java
+++ section2/Variables.java
@@ -1,8 +1,12 @@
 public class Variables{
     public static void main(String[] args) {
         
-        int passengers = 20;
+        // variables are lower camel case
+        int passengers = 5;
         System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
 
     }
 }
