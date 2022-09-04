52104d587210e39229f6640bcc7c16fd86697d31
CrimsonCoder42 <nostro37@gmail.com>
2022-09-04 11:57:47 -0400

    workbook 2.3

--- section2/MathOperations.java
+++ section2/MathOperations.java
@@ -12,33 +12,48 @@ public class MathOperations {
 
 
         // 1
-        System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
+        // System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
 
-        System.out.println("There are " + (starsInMilkyWay + starsInAndomeda) + " stars in the Milky Way and Andromeda galaxies.");
+        // System.out.println("There are " + (starsInMilkyWay + starsInAndomeda) + " stars in the Milky Way and Andromeda galaxies.");
 
-        System.out.println("With bonus marks, Ron scored " + (testScore + bonusMarks) + " /10 on his potions test. Snape wasn't too thrilled");
+        // System.out.println("With bonus marks, Ron scored " + (testScore + bonusMarks) + " /10 on his potions test. Snape wasn't too thrilled");
 
         // 2
 
-        System.out.println("Fred has " + (bagOfSweets2 - bagOfSweets1) + " more sweets than George");
+        // System.out.println("Fred has " + (bagOfSweets2 - bagOfSweets1) + " more sweets than George");
 
-        System.out.println("The Andromeda Galaxy has " + (starsInAndomeda - starsInMilkyWay) + " more stars than the Milky Way.");
+        // System.out.println("The Andromeda Galaxy has " + (starsInAndomeda - starsInMilkyWay) + " more stars than the Milky Way.");
 
-        System.out.println("Snape ended up removing marks. Ron actually scored " + (bonusMarks - testScore ) + " on his potions test");
+        // System.out.println("Snape ended up removing marks. Ron actually scored " + (bonusMarks - testScore ) + " on his potions test");
 
         // 3
 
-        System.out.println("Fred and George used a special spell to multiply their sweets to: " + (bagOfSweets2 * bagOfSweets1));
+        // System.out.println("Fred and George used a special spell to multiply their sweets to: " + (bagOfSweets2 * bagOfSweets1));
 
-        System.out.println("Snape made an error on his excel, accidentally multiplying Ron's score to: " + (bonusMarks * testScore ) + " /10");
+        // System.out.println("Snape made an error on his excel, accidentally multiplying Ron's score to: " + (bonusMarks * testScore ) + " /10");
 
         // 4 
 
-        System.out.println("Originally, Fred had " + (bagOfSweets2 / bagOfSweets1) + " times more sweets than George");
+        // System.out.println("Originally, Fred had " + (bagOfSweets2 / bagOfSweets1) + " times more sweets than George");
 
-        System.out.println("Andromeda has " + (starsInAndomeda / starsInMilkyWay) + " times more stars.");
+        // System.out.println("Andromeda has " + (starsInAndomeda / starsInMilkyWay) + " times more stars.");
 
-        System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks));
+        // System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks));
+
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
+
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
+
+        int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        System.out.println("I count: " + counter);
+
+        counter--;
+        counter--;
+        counter--;
+        System.out.println("I count: " + counter);
 
 
     }
