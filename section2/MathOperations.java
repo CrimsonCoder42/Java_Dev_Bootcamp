d15624726e0315821fac1a109ed2981d13d4d835
CrimsonCoder42 <nostro37@gmail.com>
2022-09-04 11:33:52 -0400

    math operations 1

---
+++ section2/MathOperations.java
@@ -0,0 +1,71 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        
+        int bagOfSweets1 = 5;
+        int bagOfSweets2 = 10; 
+
+        long starsInMilkyWay = 250000000000L;
+        long starsInAndomeda = 1000000000000L;
+
+        double testScore = 6.7;
+        double bonusMarks = 2.5;
+
+
+        // 1
+        System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
+
+        System.out.println("There are " + (starsInMilkyWay + starsInAndomeda) + " stars in the Milky Way and Andromeda galaxies.");
+
+        System.out.println("With bonus marks, Ron scored " + (testScore + bonusMarks) + " /10 on his potions test. Snape wasn't too thrilled");
+
+        // 2
+
+        System.out.println("Fred has " + (bagOfSweets2 - bagOfSweets1) + " more sweets than George");
+
+        System.out.println("The Andromeda Galaxy has " + (starsInAndomeda - starsInMilkyWay) + " more stars than the Milky Way.");
+
+        System.out.println("Snape ended up removing marks. Ron actually scored " + (bonusMarks - testScore ) + " on his potions test");
+
+        // 3
+
+        System.out.println("Fred and George used a special spell to multiply their sweets to: " + (bagOfSweets2 * bagOfSweets1));
+
+        System.out.println("Snape made an error on his excel, accidentally multiplying Ron's score to: " + (bonusMarks * testScore ) + " /10");
+
+
+    }
+
+
+}
+
+// Fred and George collected <result> sweets
+
+// There are <result> stars in the Milky Way and Andromeda galaxies.
+
+// With bonus marks, Ron scored <result>/10 on his potions test. Snape wasn't too thrilled
+
+
+
+// Fred has  <result>  more sweets than George
+
+// The Andromeda Galaxy has <result> more stars than the Milky Way
+
+// Snape ended up removing marks. Ron actually scored <result> on his potions test
+
+
+
+// Fred and George used a special spell to multiply their sweets to: <result>
+
+// Snape made an error on his excel, accidentally multiplying Ron's score to: <result>/10
+
+
+
+// Originally, Fred had  <result> times more sweets than George
+
+// Andromeda has <result> times more stars
+
+// Snape caught the error. Furious, he divided Ron's score to: <result>
+
+// 10 is an even number, since dividing 10 by 2 has a remainder of: <remainder>
+
+// 5 is an odd number, since dividing 5 by 2 has a remainder of: <remainder>
