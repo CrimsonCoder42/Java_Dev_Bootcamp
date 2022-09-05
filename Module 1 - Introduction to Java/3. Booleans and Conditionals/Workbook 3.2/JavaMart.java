2285677516cd25f26acc4d6725b55295ad0ba5b9
CrimsonCoder42 <nostro37@gmail.com>
2022-09-05 14:34:51 -0400

    workbook 3.3

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -11,6 +11,13 @@ public class JavaMart {
 
         //else:  Sorry, I only have <wallet> left.
 
+        if(wallet >= toyCar) {
+            System.out.println("Sure!");
+            wallet -= toyCar;
+        } else {
+            System.out.println("Sorry, I only have" + wallet + " left.");
+        }
+
         
         double nike = 95.99;       //run test case with 89.99
         System.out.println("Can I get these nike shoes?");
@@ -19,6 +26,12 @@ public class JavaMart {
             //       pay for the nike shoes 
 
         //else: Sorry, I only have <wallet> left.
+        if(wallet >= nike) {
+            System.out.println("Sure!");
+            wallet -= nike;
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
 
     }
 }
--- section3/IfElse.java
+++ section3/IfElse.java
@@ -1,13 +1,15 @@
 public class IfElse {
     public static void main(String[] args) {
-        int grade = 40;
+        int grade = 65;
 
-        if (grade >= 40) {
-
-            System.out.println("Great job, you passed!");
-
-        } else {
-            System.out.println("Study hard next time!");
+        if (grade >= 80) {
+            System.out.println("YOU GOT AN A!");
+        } else if (grade >= 70) {
+            System.out.println("YOU GOT AN B!");
+        }else if (grade >= 60) {
+            System.out.println("YOU GOT AN C!");
+        }else {
+            System.out.println("YOU GOT AN D!");
         }
     }
 }
