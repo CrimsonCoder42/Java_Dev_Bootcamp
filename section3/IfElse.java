d19e6fb7ab213c33cbfa220d10c18a8531f91e99
CrimsonCoder42 <nostro37@gmail.com>
2022-09-05 10:52:49 -0400

    workbook 3.2

--- section3/IfElse.java
+++ section3/IfElse.java
@@ -1,5 +1,13 @@
 public class IfElse {
     public static void main(String[] args) {
-        
+        int grade = 40;
+
+        if (grade >= 40) {
+
+            System.out.println("Great job, you passed!");
+
+        } else {
+            System.out.println("Study hard next time!");
+        }
     }
 }
