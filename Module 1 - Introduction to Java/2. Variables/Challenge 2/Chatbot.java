c9558d89b6c408ae3d6dce9db63c047a13325344
CrimsonCoder42 <nostro37@gmail.com>
2022-09-05 08:35:07 -0400

    next line fix

--- Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -30,10 +30,11 @@ public class Chatbot {
         System.out.println("This means I'm " + ( 400 / age) + " times older than you.");
         System.out.println("\nEnough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
+        scan.nextLine(); 
         String language = scan.nextLine();
 
         //add new a line here.
-        System.out.println("\n" + language + ", that's great! Nice chatting with you " + name + ". I have to log off now. See ya!");
+        System.out.println(language + ", that's great! Nice chatting with you " + name + ". I have to log off now. See ya!");
         
         //close scanner. 
         scan.close();
