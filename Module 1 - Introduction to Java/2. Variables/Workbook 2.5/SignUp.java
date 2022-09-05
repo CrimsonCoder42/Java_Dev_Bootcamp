47686021df7ee6bacedd8496fc75e069d30b3bf1
CrimsonCoder42 <nostro37@gmail.com>
2022-09-05 07:55:58 -0400

    section 5

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,22 +11,46 @@ public class SignUp {
         //Task 1 - Using Scanner, ask the user questions
 
         //Ask for their first name.
+        System.out.println("\nWhat is your first name? ");
+        String fname = scan.nextLine();
+
         //Ask for their last name.
+        System.out.println("\nWhat is your last name? ");
+        String lname = scan.nextLine();
+
         //Ask: how old are you?
+        System.out.println("\nHow old are you? ");
+        String age = scan.nextLine();
+
         /* Ask them to make a username. SIDE NOTE:
            – nextLine() gets "skipped" if you put it ahead of nextInt() , nextDouble(), nextLong().
            – You will understand why this happens when we cover Delimiters in the next section (Booleans and Conditionals).
            – For now, just know that the solution is to add an extra nextLine().
         */
-        //Ask what city they live in.
-        //Ask what country that's from.
+        System.out.println("\nPlease create a username. ");
+        String username = scan.nextLine();
 
+
+        //Ask what city they live in.
+        System.out.println("\nWhat city are you from? ");
+        String city = scan.nextLine();
+
+        //Ask what country that's from.
+        System.out.println("\nWhat country are you from? ");
+        String country = scan.nextLine();
 
         //Task 2 - Print their information. 
 
         System.out.println("Thank you for joining JavaGram!");
 
         //Print their information like so:
+        System.out.println("First Name: " + fname);
+        System.out.println("Last Name: " + lname);
+        System.out.println("Age: " + age);
+        System.out.println("Username: " + username);
+        System.out.println("City: " + city);
+        System.out.println("Country: " + country);
+
 
         // Your information:
         //        First Name: Rayan
@@ -39,5 +63,7 @@ public class SignUp {
 
 
         //close scanner. It's good practice :D ! 
+
+        scan.close();
     }
 }
