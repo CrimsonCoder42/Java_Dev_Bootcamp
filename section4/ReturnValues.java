1459a93c0fa56b979b44ee22d4dc704e1333d033
CrimsonCoder42 <nostro37@gmail.com>
2022-09-08 07:34:58 -0400

    Doc comments and functions

--- section4/ReturnValues.java
+++ section4/ReturnValues.java
@@ -1,23 +1,29 @@
 public class ReturnValues {
     public static void main(String[] args) {
         
-        double measure1 = measureRectangle(4.3, 2.2);
-        double measure2 = measureRectangle(3.2, 4.1);
-        double measure3 = measureRectangle(3.5, 1.2);
-        double measure4 = measureRectangle(1.6, 5.6);
-        double measure5 = measureRectangle(2.2, 4.3);
+        double measure1 = measureRectangle(4.3, 2.2, "area");
+        double measure2 = measureRectangle(3.2, 4.1, "perimeter");
 
-        stringPrinter(4.3, 2.2, measure1);
+        stringPrinter(4.3, 2.2, measure1, "area");
+        stringPrinter(4.3, 2.2, measure2, "perimeter");
 
     }
 
-    public static double measureRectangle(double length, double width) {
-        double area = length * width;
-        return area;  
+    public static double measureRectangle(double length, double width, String option) {
+        if (length < 0 || width < 0) {
+            System.out.println("length or width cannot be negative.");
+        }
+
+        switch (option){
+            case "area": return length * width;
+            case "perimeter": return 2 * (length + width);
+            default: return 404;
+        } 
+         
     }
 
-    public static void stringPrinter(double length, double width, double area){
-        System.out.println("The area of a rectangle with the width of: " + width + " and a length of " + length + " comes to: " + area );
+    public static void stringPrinter(double length, double width, double area, String option){
+        System.out.println("The " + option + " of a rectangle with the width of: " + width + " and a length of " + length + " comes to: " + area );
     }
 
 }
