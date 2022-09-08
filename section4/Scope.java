7c54c834db2cfc05617d459d6e16863b9ccebf4f
CrimsonCoder42 <nostro37@gmail.com>
2022-09-08 10:23:27 -0400

    Built in functions

---
+++ section4/Scope.java
@@ -0,0 +1,14 @@
+public class Scope {
+    
+    static int dogs = 5;
+    public static void main(String[] args) {
+        System.out.println(dogs);
+        someFunction();
+
+    }
+    public static void someFunction() {
+        
+       
+    }
+
+}
