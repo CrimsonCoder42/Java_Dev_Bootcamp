168bafd9da1d179f62b03031bdef9f197d2a41b2
CrimsonCoder42 <nostro37@gmail.com>
2022-09-13 09:27:22 -0400

    try 3

--- section2/Form.java
+++
@@ -1,9 +0,0 @@
-public class Form{
-    public static void main(String[] args) {
-        // char takes up less memory and is faster 
-        char gender = 'F';
-        
-        System.out.println("Gender: " + gender);
-
-    }
-}
--- section2/WholeNumbers.java
+++
@@ -1,9 +0,0 @@
-public class WholeNumbers {
-    public static void main(String[] args) {
-        int numberOfFans = 5000;
-        System.out.println(" There are " + numberOfFans + " fans at the Quidditch games");
-
-        long globalPopulation = 50000000000L;
-        System.out.println("The global population is: " + globalPopulation);
-    }
-}
