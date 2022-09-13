b1b7bc3f29ce0d0e09c93a27c6d31e05e2c09d32
CrimsonCoder42 <nostro37@gmail.com>
2022-09-13 09:14:21 -0400

    Back up and running

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Guess  {
     public static void main(String[] args) {
@@ -7,10 +8,23 @@ public class Guess  {
             1. Store a "secret" number between 1 and 5. 
             2. Then, prompt the user to enter a guess.
        */
+
+        Random randInt = new Random(); 
+        int randNum = randInt.nextInt((10 - 1) + 1) + 1;
+        System.out.println(randNum);
+        System.out.println("Hello World!");
+
        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
         
        Scanner scan = new Scanner(System.in);
 
+       int guessedNum = scan.nextInt();
+
+        while (randNum != guessedNum) {
+            System.out.println("Guess Again!");
+            guessedNum = scan.nextInt();
+        }
+        System.out.println("You got it!");
 
         /* Task 2 
             1. Set up a loop that keeps running while the user is incorrect
@@ -19,6 +33,7 @@ public class Guess  {
             2. Once they guess it, print: You got it!
         */        
         scan.close();
+        System.out.println("Hello World!");
     }
 
 }
